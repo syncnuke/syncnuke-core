53e4939bb1783e1d2b5aef2bd4bfa20eb7c742e7
Wolf <71544385+if-shouldrs@users.noreply.github.com>
2026-08-30 16:57:51 +0100

    Removed unfinished syncplay protocol

--- src/main/java/io/github/syncnuke/client/SyncClientFactory.java
+++ src/main/java/io/github/syncnuke/client/SyncClientFactory.java
@@ -1,7 +1,6 @@
 package io.github.syncnuke.client;
 
 import io.github.syncnuke.client.internal.protocol.datasaver.DataSaverClient;
-import io.github.syncnuke.client.internal.protocol.syncplay.SyncplayClient;
 import io.github.syncnuke.player.internal.PlayerManager;
 
 /**
@@ -12,7 +11,7 @@ class SyncClientFactory {
     /**
      * Creates a SyncClient instance based on the specified protocol.
      *
-     * @param protocol    the protocol to use (e.g., "syncplay", "datasaver")
+     * @param protocol    the protocol to use (e.g., "datasaver")
      * @param host        the host address to connect to
      * @param port        the port to connect on
      * @param videoPlayer the controlled video player instance to use with the client
@@ -21,8 +20,6 @@ class SyncClientFactory {
      */
     public static SyncClient<?> createClient(String protocol, String host, int port, PlayerManager videoPlayer) {
         switch (protocol.toLowerCase()) {
-            case "syncplay":
-                return new SyncplayClient(host, port, videoPlayer);
             case "datasaver":
                 return new DataSaverClient(host, port, videoPlayer);
             default:
--- src/main/java/io/github/syncnuke/client/internal/protocol/syncplay/service/extractor/Extractor.java
+++
@@ -1,6 +0,0 @@
-package io.github.syncnuke.client.internal.protocol.syncplay.service.extractor;
-
-@FunctionalInterface
-public interface Extractor<T, R> {
-    R extract(T source);
-}
